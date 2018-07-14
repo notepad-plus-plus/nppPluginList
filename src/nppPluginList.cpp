e11fb4da6bf785d8e17626792caa661590b617d1
Don HO <don.h@free.fr>
2018-07-14 02:50:50 +0200

    nppPluginList.dll 1st commit

---
+++ src/nppPluginList.cpp
@@ -0,0 +1,56 @@
+// This file is part of Notepad++ project
+// Copyright (C)2018 Don HO <don.h@free.fr>
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// Note that the GPL places important restrictions on "derived works", yet
+// it does not provide a detailed definition of that term.  To avoid      
+// misunderstandings, we consider an application to constitute a          
+// "derivative work" for the purpose of this license if it does any of the
+// following:                                                             
+// 1. Integrates source code from Notepad++.
+// 2. Integrates/includes/aggregates Notepad++ into a proprietary executable
+//    installer, such as those produced by InstallShield.
+// 3. Links to a library or executes a program that does any of the above.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+
+#include "resource.h"
+#include "nppPluginList.h"
+
+PluginList::PluginList(HMODULE handle) : _moduleHandle(handle) 
+{
+	DWORD size = 0;
+	const char* data = NULL;
+	loadFileInResource(IDR_PLUGINLISTJSONFILE, TEXTFILE, size, data);
+
+	_buffer = new char[size + 1];
+	::memcpy(_buffer, data, size);
+	_buffer[size] = '\0';
+}
+
+void PluginList::loadFileInResource(int name, int type, DWORD& size, const char*& data)
+{
+	HRSRC rc = ::FindResource(_moduleHandle, MAKEINTRESOURCE(name), MAKEINTRESOURCE(type));
+	HGLOBAL rcData = ::LoadResource(_moduleHandle, rc);
+	size = ::SizeofResource(_moduleHandle, rc);
+	data = static_cast<const char*>(::LockResource(rcData));
+}
+
+PluginList::~PluginList()
+{
+	if (_buffer != nullptr)
+		delete _buffer;
+}
+
