322f735c9c75c43de9507fa8ed0f087292fc5bdb
Don HO <don.h@free.fr>
2018-11-10 23:38:27 +0100

    NppPluginList first release version 1.0

--- src/resource.h
+++ src/resource.h
@@ -26,12 +26,16 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
+#pragma once
 
 #ifdef _WIN64
 #define PLJSON "pl.x64.json"
 #else
 #define PLJSON "pl.x86.json"
 #endif
+
+#define VERSION_VALUE "1.0\0"
+#define VERSION_DIGITALVALUE 1, 0, 0, 0
 
 #define TEXTFILE        256
 #define IDR_PLUGINLISTJSONFILE  101
