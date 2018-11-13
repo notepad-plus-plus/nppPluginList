ed6c0e5dd5183bacce00d36f8e3a04711d08b8a4
Don HO <don.h@free.fr>
2018-11-13 03:37:56 +0100

    NppPlugin 1.0.2 release

--- src/resource.h
+++ src/resource.h
@@ -34,8 +34,8 @@
 #define PLJSON "pl.x86.json"
 #endif
 
-#define VERSION_VALUE "1.01\0"
-#define VERSION_DIGITALVALUE 1, 0, 1, 0
+#define VERSION_VALUE "1.02\0"
+#define VERSION_DIGITALVALUE 1, 0, 2, 0
 
 #define TEXTFILE        256
 #define IDR_PLUGINLISTJSONFILE  101
