0c97637d4e4fdf2e6da6834bbc40ed7be8714c9a
Christian Grasser <christian.grasser@live.de>
2018-12-26 21:04:41 +0100

    NppPlugin 1.0.4 release

--- src/resource.h
+++ src/resource.h
@@ -34,8 +34,8 @@
 #define PLJSON "pl.x86.json"
 #endif
 
-#define VERSION_VALUE "1.03\0"
-#define VERSION_DIGITALVALUE 1, 0, 3, 0
+#define VERSION_VALUE "1.04\0"
+#define VERSION_DIGITALVALUE 1, 0, 4, 0
 
 #define TEXTFILE        256
 #define IDR_PLUGINLISTJSONFILE  101
