531699cc53ff0b7bb7346d0d7b13972257d96c16
Christian Grasser <christian.grasser@live.de>
2020-01-29 23:22:33 +0100

    Notepad++ Plugin List Release 1.1.7

--- src/resource.h
+++ src/resource.h
@@ -34,8 +34,8 @@
 #define PLJSON "pl.x86.json"
 #endif
 
-#define VERSION_VALUE "1.16\0"
-#define VERSION_DIGITALVALUE 1, 1, 6, 0
+#define VERSION_VALUE "1.17\0"
+#define VERSION_DIGITALVALUE 1, 1, 7, 0
 
 #define TEXTFILE        256
 #define IDR_PLUGINLISTJSONFILE  101
