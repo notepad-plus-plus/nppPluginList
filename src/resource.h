cff59973ddb0a05605ce1bebcce2c21f39831d6b
Don HO <don.h@free.fr>
2025-10-17 16:02:18 +0200

    Notepad++ Plugin List Release 1.8.7

--- src/resource.h
+++ src/resource.h
@@ -40,8 +40,8 @@
 #define PLJSON "pl.x86.json"
 #endif
 
-#define VERSION_VALUE "1.86\0"
-#define VERSION_DIGITALVALUE 1, 8, 6, 0
+#define VERSION_VALUE "1.87\0"
+#define VERSION_DIGITALVALUE 1, 8, 7, 0
 
 #define TEXTFILE        256
 #define IDR_PLUGINLISTJSONFILE  101
